3f19a3ec59f42c8216930dc3e1f2f63c86ade913
gurpreet <gurpreet212004@gmail.com>
2021-09-02 18:30:10 -0400

    update Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header"
+import Show from "./Show"
+import Empty from "./Empty"
+import "./styles.scss";
+
+
+
+export default function Appointment(props) {
+  return(
+    <article className="appointment">
+      <Header time={props.time}></Header>
+
+      {props.interview?<Show student={props.interview.student} interviewer={props.interview.interviewer}/> 
+      : <Empty onAdd = {props.onAdd}/>}
+    </article>
+  );
+}
---
+++ src/components/DayListItem.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import "components/DayListItem.scss";
+
+import classnames from 'classnames'; 
+
+export default function DayListItem(props) {
+
+  const formatSpots = () => {
+    
+    let spotText;
+    if(props.spots === 0) {
+      spotText = "no spots remaining";
+    } else if (props.spots ===1) {
+      spotText = "1 spot remaining";
+    } else {
+      spotText = props.spots + " spots remaining";
+    }
+      return spotText;
+
+  }
+   
+
+  const buttonClass1 = classnames("day-list__item", {
+    "day-list__item--selected": props.selected, 
+    "day-list__item--full": props.spots === 0
+});
+
+  return (
+    <li className = {buttonClass1} onClick={props.setDay}>
+    <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  );
+  
+}
+
+
